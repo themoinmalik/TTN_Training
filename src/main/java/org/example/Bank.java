871782586c545aac797a478624ef4ac2c4488572
Moin Malik <Moin Malik>
2023-02-04 00:02:03 +0530

    4 feb

--- src/main/java/org/example/Bank.java
+++ src/main/java/org/example/Bank.java
@@ -1,16 +1,115 @@
 package org.example;
 
-public class Bank {
+import javax.naming.InsufficientResourcesException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Locale;
+import java.util.Scanner;
 
+
+class InsufficientAmountException extends  Exception {
+
+    public InsufficientAmountException(String s) {
+        super(s);
+    }
+
+}
+
+class Bank {
 
     String name;
-    String rateOfInterest;
+    double rateOfInterest;
     double balance = 0.0;
+    public enum type {SAVINGS, CURRENT};
 
 
+    public String getDetails() {
+        return "Name " + name + " Rate of interest is " + rateOfInterest;
+    }
+
+    public double deposit(int amount) {
+        this.balance = this.balance + amount;
+        return balance;
+    }
+
+    public double getBalance() {
+        return  this.balance;
+    }
+
+    public double withDraw(int in) throws InsufficientAmountException {
+
+        try {
+            if (in > this.balance) {
+                throw new InsufficientAmountException("Your balance is " + this.balance);
+            } else {
+                this.balance = this.balance - in;
+            }
+        } catch (InsufficientAmountException e) {
+            System.out.println(e);
+        }
+
+        return balance;
+
+    }
+
+    public void fileLogBank(double balance, double amount) {
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        this.balance = this.balance - amount;
+
+        try {
+            FileWriter fileWriter = new FileWriter("logfile.txt");
+            fileWriter.write("Time is " + localDateTime + "\n");
+            fileWriter.write("amount is " + amount + "\n");
+            fileWriter.write("Balance is + "  +balance);
+            fileWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    static class SBI extends Bank {
+
+        public SBI() {
+            super.name = "State Bank Of India";
+            super.rateOfInterest = 5.4;
+        }
+
+        public void printDetails() {
+            String str = getDetails();
+            System.out.println(str);
+        }
+    }
 
 
+    static class BOI extends Bank {
 
+        public BOI() {
+            super.name = "Bank Of India";
+            super.rateOfInterest = 5;
+        }
+
+        public void printDetails() {
+            System.out.println(" name is " + name + "rate is " + rateOfInterest);
+        }
+    }
+
+
+    static class ICICI extends Bank {
+
+        public ICICI() {
+            super.name = "ICICI Bank Of India";
+            super.rateOfInterest = 5.5;
+        }
+
+        public void printDetails() {
+            String str = getDetails();
+            System.out.println(str);
+        }
+    }
 
 
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,49 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InsufficientAmountException {
+//        System.out.println("Hello world!");
+//
+//        String name = "Moin Malik";
+//        int age = 23;
+//        String designation = "hello worlds";
+//
+//        Employee emp = new Employee(name, age, designation);
+//
+//        System.out.println(emp.getAge());
+//        System.out.println(emp.getName());
+//        emp.setName("Malik Malik");
+//        System.out.println(emp.getName());
+//        System.out.println(emp.toString());
 
-        String name = "Moin Malik";
-        int age = 23;
-        String designation = "hello worlds";
 
-        Employee emp = new Employee(name, age, designation);
+        Bank.SBI b1 = new Bank.SBI();
+        b1.deposit(1000);
+        b1.printDetails();
+        b1.withDraw(500);
+        b1.deposit(3000);
+        System.out.println(b1.getBalance());
 
-        System.out.println(emp.getAge());
-        System.out.println(emp.getName());
-        emp.setName("Malik Malik");
-        System.out.println(emp.getName());
-        System.out.println(emp.toString());
+
+        System.out.println("-------------------------");
+
+//
+        Bank.BOI b2 = new Bank.BOI();
+        b2.printDetails();
+        b2.deposit(2000);
+        b2.withDraw(200);
+        System.out.println(b2.getBalance());
+
+
+        Bank.ICICI b3  = new Bank.ICICI();
+
+        b3.printDetails();
+
+
+        System.out.println("********************Log file ***********************");
+        Bank bank = new Bank();
+        bank.fileLogBank(2000,1000);
+
 
     }
 }
