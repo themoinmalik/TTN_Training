a68f522527c9f5a3cd8e70bdb5184973da3ce0e6
Moin Malik <Moin Malik>
2023-02-06 13:35:36 +0530

    6 feb

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.util.Objects;
+
+public class User {
+
+    String firstName;
+    String lastName;
+    int age;
+
+    // constructor...
+
+
+    public User(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    // getter setter...
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
