fd9ecb2362f66129c4b16a988630c4627901f40a
Moin Malik <Moin Malik>
2023-02-08 23:14:29 +0530

    8 feb

--- src/main/java/org/example/Java8.java
+++ src/main/java/org/example/Java8.java
@@ -4,7 +4,12 @@ package org.example;
 
 interface Calculate {
 
-    void sum();
+//    void sum();
+
+//    void sum(int n);
+
+
+    int sum(int a, int b);
 
 }
 
@@ -16,12 +21,31 @@ public class Java8 {
 
 
         /// lambda Expression...
+//
+//        Calculate calculate = () -> {
+//            System.out.println("sum is = 10");
+//        };
 
-        Calculate calculate = () -> {
-            System.out.println("sum is = 10");
-        };
+//        calculate.sum();
 
-        calculate.sum();
+
+        // parameterised...
+
+//        Calculate calculate = (n) -> {
+//            System.out.println("sum is "  + n);
+//        };
+//        calculate.sum(23);
+
+
+        // return type...
+
+//        Calculate calculate = (a,b) -> {
+//            return a+b;
+//        };
+//        System.out.println(calculate.sum(2,5));
+
+
+
 
 
     }
