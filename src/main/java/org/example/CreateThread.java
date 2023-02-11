c5c91270ca118d1ff7b9b849855aa8480bf672de
Moin Malik <Moin Malik>
2023-02-11 11:08:16 +0530

    11 feb

---
+++ src/main/java/org/example/CreateThread.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class CreateThread {
+
+    public static void main(String[] args) {
+
+        Runnable mythread = () -> {
+            Thread.currentThread().setName("my Thread... ");
+            String name = Thread.currentThread().getName();
+            System.out.println(name + "Running....");
+        };
+
+        Thread run = new Thread(mythread);
+        run.start();
+
+
+    }
+
+
+}
