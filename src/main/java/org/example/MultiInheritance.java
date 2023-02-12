7b074db1c57bd849067072b629c03c88cf5e9539
Moin Malik <Moin Malik>
2023-02-12 11:10:59 +0530

    12 feb

--- src/main/java/org/example/MultiInheritance.java
+++ src/main/java/org/example/MultiInheritance.java
@@ -8,10 +8,41 @@ interface Hello {
     }
 }
 
+class C {
+    public void cMethod() {
+        System.out.println("Hello from class C");
+    }
+}
 
-public class MultiInheritance {
 
+class A implements Hello {
 
-
+    public void method() {
+        System.out.println(" hello from class A");
+    }
 
 }
+
+class B implements Hello {
+
+    public void method() {
+        System.out.println("HEllo from class B");
+    }
+
+}
+
+
+public class MultiInheritance extends C{
+
+    public static void main(String[] args) {
+
+        A a = new A();
+        a.method();
+
+        C c = new C();
+        c.cMethod();
+
+    }
+
+}
+
